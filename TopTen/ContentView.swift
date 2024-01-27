af7e3918f3b4fe05b7354df3de5e92ac066211ac
takuto277 <takuto.72.soccer@gmail.com>
2024-01-27 16:59:26 +0900

    Initial Commit

---
+++ TopTen/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  TopTen
+//
+//  Created by 小野拓人 on 2024/01/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("hoge")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TopTen/TopTenApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopTenApp.swift
+//  TopTen
+//
+//  Created by 小野拓人 on 2024/01/27.
+//
+
+import SwiftUI
+
+@main
+struct TopTenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
