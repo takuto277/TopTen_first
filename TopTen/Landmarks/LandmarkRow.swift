a80ff3eb7575c6a7ae2e1bd36e20e7404916e7c1
takuto277 <takuto.72.soccer@gmail.com>
2024-01-31 03:04:29 +0900

    tutorials 3

--- TopTen/Landmarks/LandmarkRow.swift
+++ TopTen/Landmarks/LandmarkRow.swift
@@ -17,19 +17,18 @@ struct LandmarkRow: View {
             Text(landmark.name)
             
             Spacer()
+            
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundStyle(.yellow)
+            }
         }
     }
 }
 
-#Preview("Turtle Rock") {
-    LandmarkRow(landmark: landmarks[0])
-}
-
-#Preview("Salmon") {
-    LandmarkRow(landmark: landmarks[1])
-}
 #Preview {
-    Group {
+    let landmarks = ModelData().landmarks
+    return Group {
         LandmarkRow(landmark: landmarks[0])
         LandmarkRow(landmark: landmarks[1])
     }
--- TopTen/TopTenApp.swift
+++ TopTen/TopTenApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct TopTenApp: App {
+    @State private var modelData = ModelData()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(modelData)
+            
         }
     }
 }
