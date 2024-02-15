5149f69b503cc977405b067e1de60bd933517849
takuto277 <takuto.72.soccer@gmail.com>
2024-02-16 01:44:50 +0900

    fix layout of FeedbackView

--- TopTen/FeedbackView.swift
+++ TopTen/FeedbackView.swift
@@ -11,85 +11,82 @@ struct FeedbackView: View {
     let randomNumber = Int.random(in: 1...10)
     @State private var text: String = ""
     @State private var isEditing = false
-    @Environment(\.dismiss) var dismiss
+    @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
         GeometryReader { geometry in
-            NavigationView {
-                VStack {
-                    Text("ゲームを遊んでくれてありがとう！\n今回の回答理由があれば教えてね")
-                        .padding()
-                        .border(.black)
-                    VStack(alignment: .leading) {
-                        VStack(alignment: .leading) {
-                            Text("お題")
-                                .frame(width: geometry.size.width * 0.2, height: geometry.size.height * 0.05)
-                                .border(.black, width: 1)
-                            Text("恋人にクリスマスプレゼントをもらう。もらっても嬉しくない。もらったら嬉しい。")
-                                .frame(width: geometry.size.width * 0.9, height: geometry.size.height * 0.1)
-                                .border(.black, width: 1)
-                        }
-                        
-                        VStack(alignment: .leading) {
-                            Text("回答内容")
-                                .frame(width: geometry.size.width * 0.2, height: geometry.size.height * 0.05)
-                                .border(.black, width: 1)
-                            
-                            HStack {
-                                Text("彼氏が高校生の頃から貯金して貯めたお金で買った財布")
-                                    .frame(width: geometry.size.width * 0.7, height: geometry.size.height * 0.1)
-                                    .border(.black, width: 1)
-                                Text(String(randomNumber))
-                                    .font(.custom("STBaoliTC-Regular", size: 50))
-                                    .frame(width: geometry.size.width * 0.2, height: geometry.size.height * 0.1)
-                                    .border(Color.black, width: 3)
-                                    .background(Color(red: 1.0 - Double(randomNumber) / 10.0, green: Double(randomNumber) / 10.0, blue: 0))
-                                    .cornerRadius(6)
-                            }
+            VStack(spacing: 20) {
+                Text("ゲームを遊んでくれてありがとう！\n今回の回答理由があれば教えてね")
+                    .padding()
+                    .font(.headline)
+                    .foregroundColor(.black)
+                    .multilineTextAlignment(.center)
+                    .background(Color.yellow.opacity(0.3))
+                    .cornerRadius(10)
+                
+                VStack(alignment: .leading, spacing: 10) {
+                    Group {
+                        Text("お題")
+                            .foregroundColor(.black)
+                            .font(.headline)
+                            .padding(.horizontal, 10)
+                        Text("恋人にクリスマスプレゼントをもらう。もらっても嬉しくない。もらったら嬉しい。")
+                            .foregroundColor(.black)
+                            .padding(.horizontal, 10)
+                    }
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .shadow(color: Color.gray.opacity(0.4), radius: 4, x: 0, y: 2)
+                    
+                    Group {
+                        Text("回答内容")
+                            .foregroundColor(.black)
+                            .font(.headline)
+                            .padding(.horizontal, 10)
+                        HStack {
+                            Text("彼氏が高校生の頃から貯金して貯めたお金で買った財布")
+                                .foregroundColor(.black)
+                                .padding(.horizontal, 10)
+                            Text(String(randomNumber))
+                                .font(.custom("STBaoliTC-Regular", size: 50))
+                                .frame(width: geometry.size.width * 0.2, height: geometry.size.height * 0.1)
+                                .background(Color(red: 1.0 - Double(randomNumber) / 10.0, green: Double(randomNumber) / 10.0, blue: 0))
+                                .cornerRadius(6)
+                                .foregroundColor(.white)
+                                .padding(.horizontal, 10)
                         }
                     }
-                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .shadow(color: Color.gray.opacity(0.4), radius: 4, x: 0, y: 2)
                     
-                    VStack {
-                        
+                    Group {
                         TextEditor(text: $text)
                             .padding()
-                            .frame(height: 150) // 高さを設定
-                            .background(Color.white.opacity(0.8))
-                            .cornerRadius(8.0)
-                            .border(Color.gray, width: 1)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 8)
-                                    .stroke(Color.gray, lineWidth: 1)
-                            )
-                            .overlay(
-                                Text("今回の回答理由があれば記入をお願いします。\n他プレイヤーの出題回答に使用させていただきます。")
-                                    .font(.custom("STBaoliTC-Regular", size: 15))
-                                    .foregroundColor(.gray)
-                                    .padding(.horizontal)
-                                    .opacity(text.isEmpty ? 1 : 0) // テキストが空でない場合は非表示にする
-                            )
-                        HStack {
-
-                            
-                            NavigationLink {
-                                GameView()
-                                    .navigationBarBackButtonHidden(true)
-                            } label: {
-                                Text("次へ")
-                                    .font(.custom("STBaoliTC-Regular", size: 15))
-                                    .frame(width: geometry.size.width * 0.3, height: geometry.size.height * 0.02)
-                            }
-                            .buttonStyle(MyButtonStyle())
-                        }
-                        .padding()
+                            .frame(height: 150)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .shadow(color: Color.gray.opacity(0.4), radius: 4, x: 0, y: 2)
                         
+                        Button {
+                            presentationMode.wrappedValue.dismiss()
+                        } label: {
+                            Text("次へ")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .frame(width: geometry.size.width * 0.3, height: geometry.size.height * 0.05)
+                                .background(Color.blue)
+                                .cornerRadius(10)
+                                .shadow(color: Color.gray.opacity(0.4), radius: 4, x: 0, y: 2)
+                        }
                     }
-                    .padding()
-                    
-                    Spacer()
+                    .padding(.horizontal, 10)
                 }
+                .padding()
+                
+                Spacer()
             }
+            .background(Color.gray.opacity(0.1).edgesIgnoringSafeArea(.all))
         }
     }
 }
